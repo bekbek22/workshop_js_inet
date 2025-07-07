7adc98400a29f95dea75407e94299de129145b33
bekbek22 <bekbeknakub@gmail.com>
2025-07-07 15:59:36 +0700

    del: user.js and index.js

--- app.js
+++ app.js
@@ -6,8 +6,6 @@ var logger = require('morgan');
 var cors = require('cors');
 var connectDB = require('./config/dbConfig');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
 var authRouter = require('./routes/auth');
 var adminRouter = require('./routes/admin');
 var productRouter = require('./routes/products');
@@ -30,8 +28,6 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
 app.use('/api/v1', authRouter, adminRouter, productRouter, orderRouter);
 
 // catch 404 and forward to error handler
